b627ad9de5f1a964204649fd68e07788f9db771b
yomindu Rajasooriya <yomindurajasuriya@gmail.com>
2024-09-17 19:18:02 +0530

    update: Include topbar separately

--- sathuta_fertilizer/index.php
+++ sathuta_fertilizer/index.php
@@ -64,9 +64,9 @@
     <!-- Custom Swiper CSS -->
     <link rel="stylesheet" href="css/custom-swiper.css" />
 
-    <!-- topbar CSS -->
+    <!-- Topbar CSS -->
     <link rel="stylesheet" href="css/topbar.css" />
- 
+
 </head>
 
 <body>
@@ -76,36 +76,9 @@
     </div>
     <!-- Spinner End -->
 
-    <!-- Topbar Start -->
-    <div class="container-fluid bg-dark text-light px-0 py-0">
-         <div class="row gx-0 d-none d-lg-flex align-items-center justify-content-between">
-             <div class="col-lg-4 px-5 text-start">
-                 <a href="index.php" class="navbar-brand d-flex align-items-center p-0">
-                     <img class="logosize" src="img/logocroped.png" alt="Sathuta Group Logo">
-                 </a>
-             </div>
-           
-             <div class="col-lg-8 px-5 d-flex justify-content-end">
-                 <nav class="top d-inline-flex align-items-end">
-                     <!--<a class="group mx-2">SATHUTA GROUP|</a> -->
-                     <a class="top-link" href="https://sathutaindustry.lk/">Industry</a>
-                     <a class="top-link active" href="../sathuta_fertilizer/index.php">Fertilizer</a>
-                     <a class="top-link" href="../sathuta_metal_crusher/index.php">Metal Crusher</a>
-                     <a class="top-link" href="../sathuta_construction/index.php">Construction</a>
-                     <a class="top-link" href="../sathuta_transport/index.php">Transport</a>
-                     <a class="top-link" href="../sathuta_machinery/index.php">Machinery</a>
-                 </nav>
-             
-                 <div class="h-100 d-inline-flex align-items-right">
-                 <span class="follow-text">Follow <span class="us-text">Us:</span></span>
-                     <a class="btn btn-link text-light" href=""><i class="fab fa-facebook-f"></i></a>
-                     <a class="btn btn-link text-light" href=""><i class="fab fa-linkedin-in"></i></a>
-                 </div>
-             </div>
-         </div>
-     </div>
 
-    <!-- Topbar End -->
+    <!-- Topbar -->
+    <?php include 'topbar.php'; ?>
     
 
     <!-- Navbar Start -->
--- sathuta_fertilizer/organic_pesticide.php
+++ sathuta_fertilizer/organic_pesticide.php
@@ -57,6 +57,9 @@
 
     <!-- Custom Stylesheet -->
     <link rel="stylesheet" type="text/css" href="css/sidebar.css">
+
+    <!-- Topbar CSS -->
+    <link rel="stylesheet" href="css/topbar.css" />
 </head>
 
 <body>
@@ -67,29 +70,8 @@
     <!-- Spinner End -->
 
 
-    <!-- Topbar Start -->
-    <div class="container-fluid bg-dark text-light px-0 py-2">
-        <div class="row gx-0 d-none d-lg-flex">
-            <div class="col-lg-7 px-5 text-start">
-                <div class="h-100 d-inline-flex align-items-center me-4">
-                    <span class="fa fa-phone-alt me-2"></span>
-                    <span><a href="tel:+94705999888" class="contact_2">+94 70 599 9888</a></span>
-                </div>
-                <div class="h-100 d-inline-flex align-items-center">
-                    <span class="far fa-envelope me-2"></span>
-                    <span><a href="mailto:fertilizer@sathutagroup.com" class="contact_2">fertilizer@sathutagroup.com</a></span>
-                </div>
-            </div>
-            <div class="col-lg-5 px-5 text-end">
-                <div class="h-100 d-inline-flex align-items-center mx-n2">
-                    <span style="font-size: 12px; margin-bottom: -2px;">Follow Us:</span>
-                    <a class="btn btn-link text-light" href=""><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-link text-light" href=""><i class="fab fa-linkedin-in"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Topbar End -->
+    <!-- Topbar -->
+    <?php include 'topbar.php'; ?>
 
 
     <!-- Navbar Start -->
