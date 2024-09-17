68f523ec9e8a86a070d17d456a8d59921f2b8f3c
yomindu Rajasooriya <yomindurajasuriya@gmail.com>
2024-09-17 17:01:24 +0530

    update: css separated from php file

--- sathuta_fertilizer/index.php
+++ sathuta_fertilizer/index.php
@@ -63,6 +63,9 @@
 
     <!-- Custom Swiper CSS -->
     <link rel="stylesheet" href="css/custom-swiper.css" />
+
+    <!-- topbar CSS -->
+    <link rel="stylesheet" href="css/topbar.css" />
  
 </head>
 
@@ -73,105 +76,34 @@
     </div>
     <!-- Spinner End -->
 
-       <!-- Topbar Start -->
-       <div class="container-fluid bg-dark text-light px-0 py-0">
-        <div class="row gx-0 d-none d-lg-flex align-items-center justify-content-between">
-        <div class="col-lg-4 px-5 text-start">
-            <a href="index.php" class="navbar-brand d-flex align-items-center p-0">
-                <img class="logosize" src="img/logocroped.png" alt="Sathuta Group Logo">
-            </a>
-        </div>
-        <style>
-        .logosize {
-            width: 150px;
-            height: auto; 
-        }
-        </style>
-
-
-
-
-            <div class="col-lg-8 px-5 d-flex justify-content-end">
-                <nav class="top d-inline-flex align-items-end">
-                    <!--<a class="group mx-2">SATHUTA GROUP|</a> -->
-                    <a class="top-link" href="https://sathutaindustry.lk/">Industry</a>
-                    <a class="top-link active" href="../sathuta_fertilizer/index.php">Fertilizer</a>
-                    <a class="top-link" href="../sathuta_metal_crusher/index.php">Metal Crusher</a>
-                    <a class="top-link" href="../sathuta_construction/index.php">Construction</a>
-                    <a class="top-link" href="../sathuta_transport/index.php">Transport</a>
-                    <a class="top-link" href="../sathuta_machinery/index.php">Machinery</a>
-                </nav>
-            
-                <div class="h-100 d-inline-flex align-items-right">
-                <span style="font-size: 12px; margin-bottom: -2px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Follow&nbsp;Us:</span>
-                    <a class="btn btn-link text-light" href=""><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-link text-light" href=""><i class="fab fa-linkedin-in"></i></a>
-                </div>
-            </div>
-
-        </div>
-
-
-
-    </div>
-
-        <!-- Topbar Button styles -->
-        <style>
-        .top-link {
-            border: none;
-            /* border-radius: 3px;  */
-            padding: 8px 6px;
-            font-size: 12px;
-            color: white;
-            transition: background-color 0.3s, color 0.3s;
-            white-space: nowrap;
-            border-right: 2px solid rgba(0, 0, 0, .060);
-            
-        }
-        .top-link:hover {
-            background-color: #fff;
-            color: white;
-            display: flex;
-            color: black;
-            font-weight: bold;
-        }
-        .top-link.active {
-            background-color: #fff;
-            border-right: 2px solid rgba(0, 0, 0, .060);
-            display: flex;
-            color: black;
-            font-weight: bold;
-
-        }
-        .group {
-            border: none;
-            border-radius: 3px; 
-            padding: 8px 0px;
-            font-size: 12px;
-            color: white;
-            white-space: nowrap;
-        }
-        .group:hover {
-            color: white;
-        }
-    
-        .container-fluid {
-            justify-content: space-between;
-            align-items: center;
-        }
-        .d-flex {
-            display: flex;
-            flex-wrap: nowrap;
-            align-items: center;
-        }
-
-        .d-inline-flex {
-            display: inline-flex;
-            align-items: center;
-        }
-
-        </style>
-        <!-- Topbar Button styles -->
+    <!-- Topbar Start -->
+    <div class="container-fluid bg-dark text-light px-0 py-0">
+         <div class="row gx-0 d-none d-lg-flex align-items-center justify-content-between">
+             <div class="col-lg-4 px-5 text-start">
+                 <a href="index.php" class="navbar-brand d-flex align-items-center p-0">
+                     <img class="logosize" src="img/logocroped.png" alt="Sathuta Group Logo">
+                 </a>
+             </div>
+           
+             <div class="col-lg-8 px-5 d-flex justify-content-end">
+                 <nav class="top d-inline-flex align-items-end">
+                     <!--<a class="group mx-2">SATHUTA GROUP|</a> -->
+                     <a class="top-link" href="https://sathutaindustry.lk/">Industry</a>
+                     <a class="top-link active" href="../sathuta_fertilizer/index.php">Fertilizer</a>
+                     <a class="top-link" href="../sathuta_metal_crusher/index.php">Metal Crusher</a>
+                     <a class="top-link" href="../sathuta_construction/index.php">Construction</a>
+                     <a class="top-link" href="../sathuta_transport/index.php">Transport</a>
+                     <a class="top-link" href="../sathuta_machinery/index.php">Machinery</a>
+                 </nav>
+             
+                 <div class="h-100 d-inline-flex align-items-right">
+                 <span style="font-size: 12px; margin-bottom: -2px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Follow&nbsp;Us:</span>
+                     <a class="btn btn-link text-light" href=""><i class="fab fa-facebook-f"></i></a>
+                     <a class="btn btn-link text-light" href=""><i class="fab fa-linkedin-in"></i></a>
+                 </div>
+             </div>
+         </div>
+     </div>
 
     <!-- Topbar End -->
     
