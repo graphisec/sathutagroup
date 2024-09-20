ef4d53bf420b053afc68feacda6e2d17ca29b33b
Hiran <hirantharuka9999@gmail.com>
2024-09-20 15:45:49 +0530

    carousel update

--- sathuta_fertilizer/index.php
+++ sathuta_fertilizer/index.php
@@ -59,7 +59,7 @@
     <link href="sathuta_fertilizer/css/style.css" rel="stylesheet">
 
     <!-- Swiper CSS -->
-    <link rel="stylesheet" href="css/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="../css/swiper-bundle.min.css" />
 
     <!-- Topbar CSS -->
     <link rel="stylesheet" href="../css/custom.css" />
@@ -740,67 +740,8 @@
     </div>
     <!-- Testimonial End -->
 
-
-    <!-- Footer Start -->
-    <div class="container-fluid bg-dark text-light footer mt-5 py-5 wow fadeIn" data-wow-delay="0.1s" style="background: #0f4229;">
-        <div class="container py-5">
-            <div class="row g-5">
-                <div class="col-lg-4 col-md-6">
-                    <h4 class="text-white mb-4">Our Office</h4>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>211/A, Dewamiththa Place, Heiyanthuduwa, Sri Lanka</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i><a href="tel:+94705999888" class="contact_1">+94 70 599 9888<a/></p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i><a href="mailto:fertilizer@sathutagroup.com" class="contact_1">fertilizer@sathutagroup.com</a></p>
-                    <div class="d-flex pt-2">
-                        <a class="btn btn-square btn-outline-light rounded-circle me-2" href=""><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-square btn-outline-light rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-square btn-outline-light rounded-circle me-2" href=""><i class="fab fa-youtube"></i></a>
-                        <a class="btn btn-square btn-outline-light rounded-circle me-2" href=""><i class="fab fa-linkedin-in"></i></a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <h4 class="text-white mb-4">Services</h4>
-                    <a class="btn btn-link" href="">Landscaping</a>
-                    <a class="btn btn-link" href="">Pruning plants</a>
-                    <a class="btn btn-link" href="">Urban Gardening</a>
-                    <a class="btn btn-link" href="">Garden Maintenance</a>
-                    <a class="btn btn-link" href="">Green Technology</a>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <h4 class="text-white mb-4">Quick Links</h4>
-                    <a class="btn btn-link" href="corporate_profile.php">Corporate Profile</a>
-                    <a class="btn btn-link" href="fertilizer.php">What We Do</a>
-                    <a class="btn btn-link" href="news_event.php">News & Event</a>
-                    <a class="btn btn-link" href="contact.php">Contact Us</a>
-                    <a class="btn btn-link" href="../index.php">Sathuta Group</a>
-                </div>
-                <!-- <div class="col-lg-3 col-md-6">
-                    <h4 class="text-white mb-4">Newsletter</h4>
-                    <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-                    <div class="position-relative w-100">
-                        <input class="form-control bg-light border-light w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
-                        <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
-                    </div>
-                </div> -->
-            </div>
-        </div>
-    </div>
-    <!-- Footer End -->
-
-
-    <!-- Copyright Start -->
-    <div class="container-fluid copyright py-4">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                    &copy; <a class="border-bottom" href="index.php">Sathuta Fertilizer</a>, All Right Reserved.
-                </div>
-                <div class="col-md-6 text-center text-md-end">
-                    Designed By <a class="border-bottom" href="https://aitech.lk" target="_blank">AI TECH</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Copyright End -->
+    <!-- Footer  -->
+    <?php include '../footer.php'; ?>
 
 
     <!-- Back to Top -->
