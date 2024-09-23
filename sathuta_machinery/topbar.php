f1bbb98a8988eb2cd7f866d2303075d4ca52bd2a
Hiran <hirantharuka9999@gmail.com>
2024-09-23 11:06:36 +0530

    cc

--- sathuta_machinery/topbar.php
+++ sathuta_machinery/topbar.php
@@ -9,11 +9,11 @@
         <div class="col-lg-8 px-5 d-flex justify-content-end">
             <nav class="top d-inline-flex align-items-end">
                 <a class="top-link" href="https://sathutaindustry.lk/">Industry</a>
-                <a class="top-link active" href="../sathuta_fertilizer/index.php">Fertilizer</a>
+                <a class="top-link " href="../sathuta_fertilizer/index.php">Fertilizer</a>
                 <a class="top-link " href="../sathuta_metal_crusher/index.php">Metal Crusher</a>
                 <a class="top-link" href="../sathuta_construction/index.php">Construction</a>
                 <a class="top-link" href="../sathuta_transport/index.php">Transport</a>
-                <a class="top-link" href="../sathuta_machinery/index.php">Machinery</a>
+                <a class="top-link active" href="../sathuta_machinery/index.php">Machinery</a>
             </nav>
             <div class="h-100 d-inline-flex align-items-right">
                 <span class="follow-text">Follow <span class="us-text">Us:</span></span>
